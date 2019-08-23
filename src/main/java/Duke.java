e46555f161698ac8fb501e8b840103362d3e8f62
wenhui <wenhuineo@gmail.com>
2019-08-23 23:57:32 +0800

    level 2

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.ArrayList;
 
 public class Duke {
     public static void main(String[] args) {
@@ -9,17 +10,33 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        //lvl 1 greet, echo, exit
+        //lvl 2: add, list
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
         String userInput = scan.nextLine();
         String bye = "bye";
+        String listString = "list";
+        ArrayList<String> userList = new ArrayList<>(100);
         while (!userInput.equalsIgnoreCase(bye)){
-            System.out.println(userInput);
+            if (userInput.equalsIgnoreCase(listString)){
+                printList(userList);
+            } else {
+                System.out.println("added: " + userInput);
+                userList.add(userInput);
+            }
             userInput = scan.nextLine();
         }
-        if (userInput.equalsIgnoreCase(bye)){
-            System.out.println("Bye. Hope to see you again soon!");
+        System.out.println("Bye. Hope to see you again soon!");
+    }
+    //function to print list out -- prints error message when list is empty
+    public static void printList (ArrayList<String> l){
+        if (l.isEmpty()) {
+            System.out.println("e r r o r: list is currently empty!! \npls enter any input and try again");
+        } else{
+            int count = 0;
+            for (int i = 0; i < l.size(); i++){
+                System.out.println(++count + ". " + l.get(i));
+            }
         }
     }
 }

c2c804d5a91e54d4bd8f472c633d210e24e28a50
wenhui <wenhuineo@gmail.com>
2019-08-27 12:45:14 +0800

    level 4

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -11,17 +11,18 @@ public class Duke {
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
 
-        //lvl 3: mark as done
+        //lvl 4: to dos, events, deadlines
         System.out.println("Hello! I'm Duke \nWhat can I do for you?");
         Scanner scan = new Scanner(System.in);
-        String userInput = scan.nextLine();
-        String bye = "bye";
-        String listString = "list";
+        String userInput = scan.next();
+        String status;
+        char trash;
         ArrayList<Task> userList = new ArrayList<>(100);
-        while (!userInput.equalsIgnoreCase(bye)){
-            if (userInput.equalsIgnoreCase(listString)){
+        while (!userInput.equalsIgnoreCase("bye")) {
+            if (userInput.equalsIgnoreCase("list")) {
                 printList(userList);
             } else if (userInput.contains("done")){
+                userInput = scan.nextLine();
                 String[] tokens = userInput.split(Pattern.quote(" "));
                 int taskNum = Integer.parseInt(tokens[1]) - 1;
                 if (taskNum > userList.size()){
@@ -31,27 +32,56 @@ public class Duke {
                     doneTask.markAsDone();
                     System.out.println("Nice! I've marked this task as done: \n[\u2713] " + doneTask.description);
                 }
-            } else{
-                Task newTask = new Task(userInput);
-                System.out.println("added: " + userInput);
-                userList.add(newTask);
+            } else {
+                if (userInput.equalsIgnoreCase("todo")) {
+                    userInput = scan.nextLine();
+                    Task newTodo = new Todo(userInput);
+                    System.out.println("Got it. I've added this task:\n\t[T][\u2718] " + userInput);
+                    userList.add(newTodo);
+                    System.out.println("Now you have " + userList.size() + " tasks in the list");
+                } else if (userInput.equalsIgnoreCase("deadline")) {
+                    userInput = scan.nextLine();
+                    String[] tokens = userInput.split(" /by ");
+                    Task newDeadline = new Deadline(tokens[0], tokens[1]);
+                    System.out.println("Got it. I've added this task:\n\t[D][\u2718] " + tokens[0] + " (by: " + tokens[1] + ")");
+                    userList.add(newDeadline);
+                    System.out.println("Now you have " + userList.size() + " tasks in the list");
+                } else if (userInput.equalsIgnoreCase("event")) {
+                    userInput = scan.nextLine();
+                    String[] tokens = userInput.split(" /at ");
+                    Task newEvent = new Event(tokens[0], tokens[1]);
+                    System.out.println("Got it. I've added this task:\n\t[E][\u2718] " + tokens[0] + " (at: " + tokens[1] + ")");
+                    userList.add(newEvent);
+                    System.out.println("Now you have " + userList.size() + " tasks in the list");
+                } else{
+                    System.out.println("e r r o r: this is not a valid input!!\npls try another command");
+                }
             }
-            userInput = scan.nextLine();
+            userInput = scan.next();
         }
         System.out.println("Bye. Hope to see you again soon!");
     }
+
     //function to print list out -- prints error message when list is empty
-    public static void printList (ArrayList<Task> l){
+    public static void printList(ArrayList<Task> l) {
         if (l.isEmpty()) {
             System.out.println("e r r o r: list is currently empty!! \npls enter any input and try again");
-        } else{
+        } else {
             System.out.println("Here are the tasks in your list:");
             int count = 0;
             String status;
-            for (int i = 0; i < l.size(); i++){
+            String type;
+            for (int i = 0; i < l.size(); i++) {
                 Task t = l.get(i);
                 status = t.getStatusIcon();
-                System.out.println(++count + ". [" + status + "] " + t.description);
+                type = t.getType();
+                if (type.contains("E")) {
+                    System.out.println(++count + "." + type + status + t.description + " (at: " + t.extra + ")");
+                } else if (type.contains("D")) {
+                    System.out.println(++count + "." + type + status + t.description + " (by: " + t.extra + ")");
+                } else {
+                    System.out.println(++count + "." + type + status + t.description + t.extra);
+                }
             }
         }
     }
